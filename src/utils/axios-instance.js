7db70d13b7acaa8a49732622e5eff30b93e1d062
chilsurr <suryaadevs17@gmail.com>
2026-04-01 04:33:45 +0700

    Update axios-instance.js

--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://backendbarokah-production-c3d2.up.railway.app/',
+  baseURL: 'https://backendbarokah-production-c3d2.up.railway.app',
   timeout: 1000,
   // headers: {
   //   "Content-Type": "application/json",
